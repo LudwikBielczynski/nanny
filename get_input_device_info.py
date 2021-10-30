7796ad6f22c1c0714283b26b5194ac98e591c4a6
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2021-10-30 16:59:35 +0200

    Find snd_rpi_simple_card device

--- get_input_device_info.py
+++ get_input_device_info.py
@@ -7,13 +7,13 @@ host_api_idx = default_host_api_info['index']
 # print(default_host_api_info)
 
 device_count = default_host_api_info.get('deviceCount')
-input_devices = []
+rpi_simple_card_info = None
 for device_idx in range(0, device_count):
-    max_input_channel = pyaudio.get_device_info_by_host_api_device_index(host_api_idx, device_idx) \
-                               .get('maxInputChannels')
-    if max_input_channel > 0:
-        device = pyaudio.get_device_info_by_host_api_device_index(host_api_idx, device_idx)
-        input_devices.append(device)
+    device_name = pyaudio.get_device_info_by_host_api_device_index(host_api_idx, device_idx) \
+                               .get('name')
+    if "snd_rpi_simple_card" in device_name:
+        rpi_simple_card_info = pyaudio.get_device_info_by_host_api_device_index(host_api_idx,
+                                                                                device_idx)
 
-print(input_devices)
-print(pyaudio.get_default_input_device_info())
+print(rpi_simple_card_info)
+# print(pyaudio.get_default_input_device_info())
