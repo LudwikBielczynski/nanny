f95d9b2ca1c20a52bbb731cede5238cec4f47370
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2021-10-31 20:26:57 +0100

    [microphone] Catch exception and log while runing in loop

--- save_audio.py
+++ save_audio.py
@@ -1,7 +1,14 @@
+from nanny.logger import LoggerSimple
 from nanny.microphone import Microphone
 
 if __name__ == "__main__":
+    logger = LoggerSimple()
     microphone = Microphone()
 
     while True:
-        microphone.save_locally(time_record_seconds=30)
+        try:
+            microphone.save_locally(time_record_seconds=30)
+
+        except Exception as e:
+            logger.info('Exception was raised')
+            logger.error(e)
