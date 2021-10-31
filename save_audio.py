3badbf23d8f03db8598a2acf2e4892bea62eec85
Ludwik Bielczynski <ludwik.bielczynski@yieldplanet.com>
2021-10-31 20:14:04 +0100

    [microphone] Run forever save_audio

--- save_audio.py
+++ save_audio.py
@@ -2,4 +2,6 @@ from nanny.microphone import Microphone
 
 if __name__ == "__main__":
     microphone = Microphone()
-    microphone.save_locally(time_record_seconds=60)
+
+    while True:
+        microphone.save_locally(time_record_seconds=30)
